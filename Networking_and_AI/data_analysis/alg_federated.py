07755f43d1324bfe49dcad56ec62989aaad637cf
Gorea Tudor <tudortudor2002@gmail.com>
2025-06-10 22:11:53 +0300

    repaired federated

--- Networking_and_AI/data_analysis/alg_federated.py
+++ Networking_and_AI/data_analysis/alg_federated.py
@@ -11,20 +11,12 @@ from sklearn.preprocessing import LabelEncoder
 
 from data_analysis.data_loader import load_testing_supervised_data, load_training_supervised_data
 from data_analysis.graphs import draw_confusion_matrix, draw_weights_graph
-from data_analysis.utils import SEQ_LENGTH, N_EPOCHS, LR, create_sequences
+from data_analysis.utils import SEQ_LENGTH, N_EPOCHS, LR, create_sequences, TIME_SERIES_INPUT_FEATURES
 
 
 ### Uncomment next 2 lines if multiple models can't be trained at once:
 # import tensorflow as tf
 # tf.config.run_functions_eagerly(True)
-
-
-def __encode_attacks_globally(train_dfs):
-    le_attack = LabelEncoder()
-    attacks = np.concatenate([df[df['attack_type'] != 'unknown']['attack_type'].values for df in train_dfs])
-    le_attack.fit(attacks)
-
-    return attacks, le_attack
 
 
 def __encode_protocols_globally(train_dfs):
@@ -33,6 +25,23 @@ def __encode_protocols_globally(train_dfs):
     le_protocol.fit(protocols)
 
     return protocols, le_protocol
+
+
+def __encode_flags_globally(train_dfs):
+    le_flags = LabelEncoder()
+    flags = np.concatenate([df['flags'].fillna('-').values for df in train_dfs])
+    print(f"flags {flags}")
+    le_flags.fit(flags)
+
+    return flags, le_flags
+
+
+def __encode_attacks_globally(train_dfs):
+    le_attack = LabelEncoder()
+    attacks = np.concatenate([df[df['attack_type'] != 'unknown']['attack_type'].values for df in train_dfs])
+    le_attack.fit(attacks)
+
+    return attacks, le_attack
 
 
 def __average_weights(weight_list):
@@ -44,6 +53,7 @@ def __average_weights(weight_list):
 
 def run_federated(train_dfs: list[DataFrame],
                   test_df: DataFrame,
+                  input_features: list[str] = TIME_SERIES_INPUT_FEATURES,
                   seq_len: int = SEQ_LENGTH,
                   n_epochs: int = N_EPOCHS,
                   lr: float = LR):
@@ -51,31 +61,38 @@ def run_federated(train_dfs: list[DataFrame],
     nr_clients = len(train_dfs)
     print(f"federated training using {nr_clients} clients")
 
-    # rm unknown
+    # filter unknowns
     test_df = test_df[test_df['attack_type'] != 'unknown']
 
     # encode labels globally
-    attacks, le_attack = __encode_attacks_globally(train_dfs)
     protocols, le_protocol = __encode_protocols_globally(train_dfs)
+    flags, le_flags = __encode_flags_globally(train_dfs)
+    attacks, le_attack = __encode_attacks_globally(train_dfs)
 
-    # preprocessing
+    # encode test labels
     test_df['protocol_encoded'] = le_protocol.transform(test_df['protocol'])
+    test_df['flags_encoded'] = le_flags.transform(test_df['flags'])
+    test_df['attack_type_encoded'] = le_attack.transform(test_df['attack_type'])
+
     if not set(test_df['attack_type']).issubset(set(le_attack.classes_)):
         print("Test set contains labels not present in training set. Exiting.")
         return
-    test_df['attack_type_encoded'] = le_attack.transform(test_df['attack_type'])
+
+    # sort test by time
     test_df = test_df.sort_values('time')
-    test_x = test_df[['size', 'protocol_encoded']].values
+
+    # input and output features
+    test_x = test_df[input_features].values
     test_y = test_df['attack_type_encoded'].values
+
+    # create sequences & one-hot encode labels
+    num_classes = len(le_attack.classes_)
     test_seq_x, test_seq_y = create_sequences(test_x, test_y, seq_len)
     test_seq_cat_y = to_categorical(test_seq_y, num_classes=len(le_attack.classes_))
 
-    input_shape = (seq_len, 2)
-    num_classes = len(le_attack.classes_)
-
     # create initial model
     initial_model = Sequential([
-        Input(shape=input_shape),
+        Input(shape=(seq_len, test_seq_x.shape[2])),
         Conv1D(64, kernel_size=3, activation='relu'),
         GlobalMaxPooling1D(),
         Dropout(0.3),
@@ -93,14 +110,22 @@ def run_federated(train_dfs: list[DataFrame],
     for client_idx, client_df in enumerate(train_dfs):
         print(f"\n\nClient {client_idx + 1}:")
 
-        # local preprocessing
+        # filter unknowns
         client_df = client_df[client_df['attack_type'] != 'unknown']
+
+        # encode labels
         client_df['protocol_encoded'] = le_protocol.transform(client_df['protocol'])
+        client_df['flags_encoded'] = le_flags.transform(client_df['flags'])
         client_df['attack_type_encoded'] = le_attack.transform(client_df['attack_type'])
+
+        # sort by time
         client_df = client_df.sort_values('time')
 
-        client_x = client_df[['size', 'protocol_encoded']].values
+        # input and output features
+        client_x = client_df[input_features].values
         client_y = client_df['attack_type_encoded'].values
+
+        # create sequences & one-hot encode labels
         client_seq_x, client_seq_y = create_sequences(client_x, client_y, seq_len)
         client_seq_cat_y = to_categorical(client_seq_y, num_classes=num_classes)
 
@@ -111,19 +136,22 @@ def run_federated(train_dfs: list[DataFrame],
                             loss='categorical_crossentropy',
                             metrics=['accuracy'])
 
+        # train
         print(">> Training...")
         local_model.fit(client_seq_x, client_seq_cat_y, epochs=n_epochs, batch_size=32, verbose=0)
         all_weights.append(local_model.get_weights())
 
-        # --- Evaluate this client's model on the test set ---
+        # predict
         pred_y_prob = local_model.predict(test_seq_x)
         pred_y = np.argmax(pred_y_prob, axis=1)
 
+        # decode
         test_labels_str = le_attack.inverse_transform(test_seq_y)
         pred_labels_str = le_attack.inverse_transform(pred_y)
         labels_in_test = np.unique(test_seq_y)
         target_names_in_test = le_attack.inverse_transform(labels_in_test)
 
+        # report client
         print(">> Evaluation:")
         print(classification_report(
             test_labels_str,
@@ -134,22 +162,23 @@ def run_federated(train_dfs: list[DataFrame],
 
         draw_confusion_matrix(test_labels_str, pred_labels_str, label_names=target_names_in_test)
 
-    # Average weights and create global model
+    # aggregate weights -- by averaging
     print("\n\n >>> Global Model <<<")
     global_model = clone_model(initial_model)
     averaged_weights = __average_weights(all_weights)
     global_model.set_weights(averaged_weights)
 
-    # Predict on test set
+    # predict
     pred_y_prob = global_model.predict(test_seq_x)
     pred_y = np.argmax(pred_y_prob, axis=1)
 
-    # Convert back to labels
+    # decode
     test_labels_str = le_attack.inverse_transform(test_seq_y)
     pred_labels_str = le_attack.inverse_transform(pred_y)
     labels_in_test = np.unique(test_seq_y)
     target_names_in_test = le_attack.inverse_transform(labels_in_test)
 
+    # report
     print(">> Final Results:")
     print(classification_report(
         test_labels_str,
